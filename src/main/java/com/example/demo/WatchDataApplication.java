7c792b89626d560a89b7c13ecd916e8f191b0192
Tushar Budhiraja <tushar.budhiraja@telekom-digital.com>
2023-02-07 18:02:32 +0530

    demo

--- src/main/java/com/example/demo/WatchDataApplication.java
+++ src/main/java/com/example/demo/WatchDataApplication.java
@@ -2,8 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+@EnableJpaRepositories("com.example.demo.*")
+@ComponentScan(basePackages = { "com.example.demo.*" })
+@EntityScan("com.example.demo.*")   
 public class WatchDataApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/entity/WatchData.java
+++ src/main/java/com/example/demo/entity/WatchData.java
@@ -1,5 +1,133 @@
 package com.example.demo.entity;
 
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//import jakarta.persistent.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+@Entity
+@Table(name="watchData")
 public class WatchData {
+    
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+    Integer id;
+    
+	String accountId;
+    
+    String contentType;
+    
+    long duration;
+
+	Date dateWatch;
+    
+    public WatchData(int id, String accountId, String contentType, long duration, Date dateWatch, String refId,
+			boolean isRedeemed, String coupon, String link) {
+		super();
+		this.id = id;
+		this.accountId = accountId;
+		this.contentType = contentType;
+		this.duration = duration;
+		this.dateWatch = dateWatch;
+		this.refId = refId;
+		this.isRedeemed = isRedeemed;
+		this.coupon = coupon;
+		this.link = link;
+	}
+
+	String refId;
+    
+    boolean isRedeemed;
+    
+    String coupon;
+    
+    String link;
+    
+    
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+	public Date getDateWatch() {
+		return dateWatch;
+	}
+
+	public void setDateWatch(Date dateWatch) {
+		this.dateWatch = dateWatch;
+	}
+
+	public String getRefId() {
+		return refId;
+	}
+
+	public void setRefId(String refId) {
+		this.refId = refId;
+	}
+
+	public boolean isRedeemed() {
+		return isRedeemed;
+	}
+
+	public void setRedeemed(boolean isRedeemed) {
+		this.isRedeemed = isRedeemed;
+	}
+
+	public String getCoupon() {
+		return coupon;
+	}
+
+	public void setCoupon(String coupon) {
+		this.coupon = coupon;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+    
+    
+   
+    
+
 
 }
